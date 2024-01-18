fe82e40c94c28ee967d744a9f0125fd63c5dc713
Melih <melih@MBP-von-bss.home>
2024-01-18 13:51:06 +0300

    commit

--- ios/EdgeDetector.swift
+++ ios/EdgeDetector.swift
@@ -8,19 +8,5 @@ class EdgeDetector: NSObject {
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
       
-      guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-             return
-         }
-      
-      let scannerViewController = ImageScannerController(delegate: appDelegate.self)
-             scannerViewController.modalPresentationStyle = appDelegate.window?.fullScreen
-
-             if #available(iOS 13.0, *) {
-                 scannerViewController.navigationBar.tintColor = appDelegate.window?.label
-             } else {
-                 scannerViewController.navigationBar.tintColor = appDelegate.window?.black
-             }
-
-             present(scannerViewController, animated: true)
   }
 }
